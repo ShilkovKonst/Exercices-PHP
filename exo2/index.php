4b8ba3375bc7a4f7ecbb70fd556bd788098405af
ShilkovKonst <konst.shilkov@gmail.com>
2022-12-22 16:20:34 +0100

    11

--- exo2/index.php
+++ exo2/index.php
@@ -21,20 +21,39 @@
 <!-- exercice 2 -->
 <h1>Exercice 2</h1>
 <?php
-// $myJson = '{"nickname": "BlackOrb", "password": "123456789"}';
-$array = json_decode('http://localhost/exoPHP/exo2/userpassword.json', true);
+//$myJson = '{
+//     "users": [
+//         {
+//             "nickname": "BlackOrb",
+//             "password": "123456789"
+//         },
+//         {
+//             "nickname": "DuncanGamer",
+//             "password": "987654321"
+//         }
+//     ]
+// }';
+$myjson = file_get_contents('http://localhost/php-exo/exo2/userpassword.json');
+$array = json_decode($myjson, true);
+$users = $array['users'];
+var_dump($users);
+
 if (empty($_POST['nickname']) || empty($_POST['password'])) {
     echo '<a href="login.php">Il faut renseigner votre nickname et password!</a>';
-} else if ($_POST['nickname'] === $array['nickname'] && $_POST['password'] === $array['password']) {
-    session_start();// On crée quelques variables de session dans $_SESSION
-    $_SESSION['prenom'] = $_POST['prenom']; 
-    $_SESSION['nom'] = $_POST['nom']; 
-    $_SESSION['nickname'] = $_POST['nickname'];
-    $_SESSION['password'] = $_POST['password'];
-    echo '<p>Bonjour, '.$_SESSION['prenom'].' '.$_SESSION['nom'].'!</p>';
-    echo '<p>Vous pouvez continuer votre session en visitant <a href="page1.php">page 2</a> ou vous pouvez <a href="logout.php">fermer votre session</a></p>';
-} else {
-    echo '<p>Votre nickname ou password sont invalides!</p>';
-    echo '<a href="login.php">Il faut renseigner votre nickname et password!</a>';
-}
+} else
+    foreach ($users as $user) {
+        if ($_POST['nickname'] === $user['nickname'])
+            if ($_POST['password'] === $user['password']) {
+                session_start(); // On crée quelques variables de session dans $_SESSION
+                $_SESSION['prenom'] = $_POST['prenom'];
+                $_SESSION['nom'] = $_POST['nom'];
+                $_SESSION['nickname'] = $_POST['nickname'];
+                $_SESSION['password'] = $_POST['password'];
+                echo '<p>Bonjour, ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'] . '!</p>';
+                echo '<p>Vous pouvez continuer votre session en visitant <a href="page1.php">page 2</a> ou vous pouvez <a href="logout.php">fermer votre session</a></p>';
+            } else {
+                echo '<p>Votre nickname ou password sont invalides!</p>';
+                echo '<a href="login.php">Il faut renseigner votre nickname et password!</a>';
+            }
+    }
 ?>
